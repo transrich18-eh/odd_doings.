a82c26b29daad9323b69d6ba45b55e990662f4bc
transrich18-eh <transrich18@gmail.com>
2025-08-30 03:58:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function Chodo(){
+    let chudirBhaierNam=document.getElementById('ipt').value;
+    chudirBhaierNam = chudirBhaierNam.toUpperCase()
+    document.getElementById("Output").innerText=  `"${chudirBhaierNam}" The Ultimate *CHUDIR-VHAI*`;
+}
